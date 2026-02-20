9387e127452097019d20902037c54c7a44e6e82a
Ajay Centos <ajaycentos@gmail.com>
2026-02-20 20:23:49 +0530

    Initial commit with secured storage buckets

--- storage.tf
+++ storage.tf
@@ -2,7 +2,7 @@
 # The Main Assets Bucket
 resource "google_storage_bucket" "enterprise_assets" {
   name                        = "${var.project_id}-data-bucket"
-  location                    = "US"
+  location                    = "us-east1"
   uniform_bucket_level_access = true
   public_access_prevention    = "enforced"
 
