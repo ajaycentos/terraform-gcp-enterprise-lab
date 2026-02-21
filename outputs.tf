078e77d2764d6a6c90f25d976e1c43c99db53fb3
Ajay Centos <ajaycentos@gmail.com>
2026-02-21 20:41:30 +0530

    updated output for only specific bucket url

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,10 @@ output "bucket_names" {
   value       = module.cloud_storage.names
 }
 output "bucket_alpha" {
-
-  description = "The URLs of the created storage names"
-
+  description = "The bucket alpha of the created s"
   value       = module.cloud_storage.names["test-bucket-alpha"]
-
+}
+output "bucket_alpha_url" {
+  description = "The URLs of bucket alpha"
+  value       = module.cloud_storage.urls["test-bucket-alpha"]
 }
